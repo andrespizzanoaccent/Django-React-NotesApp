2df27b977100c5b071ba91f39326a19960b06511
andrespizzanoaccent <andres.pizzano@accenture.com>
2026-02-26 18:17:03 +0100

    feat: Implement category assignment and filtering for notes – api/models.py

--- api/models.py
+++ api/models.py
@@ -1,12 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    metadata = models.JSONField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
 
 
 class Note(models.Model):
     body = models.TextField(null=True, blank=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
 
     def __str__(self):
         return self.body[0:50]
